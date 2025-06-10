85266f95208f215b97886e07c7fff62a46f9e3d7
mostafaamahmoudd <mostafaa.mahmoudd550@gmail.com>
2025-06-10 16:29:39 +0300

    fixs

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,19 @@ Route::post('/register', [App\Http\Controllers\API\AuthController::class, 'regis
 Route::post('/login', [App\Http\Controllers\API\AuthController::class, 'login']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::apiResource('posts', \App\Http\Controllers\API\PostController::class);
+    Route::apiResource('posts', App\Http\Controllers\API\PostController::class);
 
-    Route::apiResource('comments', \App\Http\Controllers\API\CommentController::class);
+    Route::post('/posts/{post}/comments', [App\Http\Controllers\API\CommentController::class, 'store']);
+    Route::put('/comments/{comment}', [App\Http\Controllers\API\CommentController::class, 'update']);
+    Route::delete('/comments/{comment}', [App\Http\Controllers\API\CommentController::class, 'destroy']);
 
-    Route::apiResource('tags', \App\Http\Controllers\API\TagController::class);
-
-    Route::apiResource('search', \App\Http\Controllers\API\SearchController::class);
+    Route::get('/tags', [App\Http\Controllers\API\TagController::class, 'index']);
+    Route::get('/tags/{tag}/posts', [App\Http\Controllers\API\TagController::class, 'getPosts']);
 
     Route::post('/logout', [App\Http\Controllers\API\AuthController::class, 'logout']);
 });
+
+Route::apiResource('search', \App\Http\Controllers\API\SearchController::class);
 
 Route::get('/posts', [\App\Http\Controllers\API\PostController::class, 'index']);
 Route::get('/posts/{post}', [\App\Http\Controllers\API\PostController::class, 'show']);
