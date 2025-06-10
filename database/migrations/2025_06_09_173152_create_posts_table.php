108bbb38e563a9d085dce272ebb6eb597af4e8a4
mostafaamahmoudd <mostafaa.mahmoudd550@gmail.com>
2025-06-10 14:30:44 +0300

    fix

--- database/migrations/2025_06_09_173152_create_posts_table.php
+++ database/migrations/2025_06_09_173152_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('content');
-            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
